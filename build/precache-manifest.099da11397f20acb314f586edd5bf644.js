bd9748cd5341cc5e22c7b1edb40b446be48db4ee
Naveen Chahar <naveenchahar2891@gmail.com>
2020-12-14 12:05:18 +0530

    This is build branch

---
+++ build/precache-manifest.099da11397f20acb314f586edd5bf644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31617c5518a7c2d954cd9f4fba4af6ad",
+    "url": "/gitPractice/index.html"
+  },
+  {
+    "revision": "7a709a6e71c3a7999fe8",
+    "url": "/gitPractice/static/css/main.521e6932.chunk.css"
+  },
+  {
+    "revision": "bf39f471e8d4e0f4eaec",
+    "url": "/gitPractice/static/js/2.66a9379c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gitPractice/static/js/2.66a9379c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a709a6e71c3a7999fe8",
+    "url": "/gitPractice/static/js/main.5ec57616.chunk.js"
+  },
+  {
+    "revision": "9eaa031eabb685212506",
+    "url": "/gitPractice/static/js/runtime-main.bc860023.js"
+  }
+]);
